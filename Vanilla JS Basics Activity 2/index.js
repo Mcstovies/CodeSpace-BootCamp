cee373a71330187bbde54bb150175cf8ff8d1da0
Mcstovies <156803436+Mcstovies@users.noreply.github.com>
2024-04-12 14:13:03 +0100

    Add files via upload

---
+++ Vanilla JS Basics Activity 2/index.js
@@ -0,0 +1,16 @@
+// Prompt the user to input the first number
+let firstNumber = prompt("Input the first number:");
+
+// Prompt the user to input the second number
+let secondNumber = prompt("Input the second number:");
+
+// Convert the input strings to numbers
+firstNumber = parseFloat(firstNumber);
+secondNumber = parseFloat(secondNumber);
+    // Perform the division
+    let result = firstNumber / secondNumber;
+    
+    // Print the result
+    console.log("The division of the first number and the second number is: " + result);
+
+
