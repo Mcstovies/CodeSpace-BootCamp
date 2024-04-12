792f43e6e39de69a5f75a117181efa7dc8a9d89d
Mcstovies <156803436+Mcstovies@users.noreply.github.com>
2024-04-12 17:17:59 +0100

    Add files via upload

---
+++ JavaScript Control Statements 1/index.js
@@ -0,0 +1,17 @@
+//Defining the number values
+let x = 2;
+let y = 3;
+let z = 4;
+
+// Checking if all numbers are equal
+if (x === y && y === z) {
+    console.log("All numbers are equal.");
+} 
+// Checking if all numbers are different
+else if (x !== y && x !== z && y !== z) {
+    console.log("All numbers are different.");
+} 
+// If neither all are equal nor different
+else {
+    console.log("Neither all are equal nor different.");
+}
