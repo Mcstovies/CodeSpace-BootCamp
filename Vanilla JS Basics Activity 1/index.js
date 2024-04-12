8556f7ab247ed7542b5fcb14aeb3d39b9d8ac705
Mcstovies <156803436+Mcstovies@users.noreply.github.com>
2024-04-12 13:30:49 +0100

    Add files via upload

---
+++ Vanilla JS Basics Activity 1/index.js
@@ -0,0 +1,14 @@
+// Calculate and print the result of the expression: -5 + 8 * 6
+console.log( -5 + 8 * 6);
+
+// Calculate and print the result of the expression: (55 + 9) % 9
+console.log((55 + 9) % 9);
+
+// Calculate and print the result of the expression: 20 + -3 * 5 / 8
+console.log(20 + -3 * 5 / 8);
+
+// Calculate and print the result of the expression: 5 + 15 / 3 * 2 - 8 % 3
+console.log(5 + 15 / 3 * 2 - 8 % 3);
+
+
+
