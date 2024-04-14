51578e072310abdb000d8a54920563d28d647395
Mcstovies <156803436+Mcstovies@users.noreply.github.com>
2024-04-14 09:16:37 +0100

    Update index.js

--- JavaScript Control Statements 2/index.js
+++ JavaScript Control Statements 2/index.js
@@ -1,15 +1,17 @@
+//Test Data
 let x = 1524
 let y = 2345
 let z = 3321
 
+// Function to check if numbers are in increasing order
 function isInIncreasingOrder(x, y, z){
     return x < y && y < z;
 }
-
+// Function to check if numbers are in decreasing order
 function isInDecreasingOrder(x, y, z){
     return x > y && y > z;
 }
-
+// Function to determine the order based on the numbers
 function determineOrder(x, y, z){
     if (isInIncreasingOrder(x, y, z)){
         return "increasing";
@@ -19,6 +21,6 @@ function determineOrder(x, y, z){
         return "Neither increasing nor decreasing order";
     }
 }
-
+// Prints the order of test data
 let order = determineOrder(x, y, z);
 console.log(order);
