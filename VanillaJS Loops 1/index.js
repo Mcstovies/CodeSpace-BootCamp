e9f8034c317aabfdf6a5e60cebb227f452284f6c
Mcstovies <156803436+Mcstovies@users.noreply.github.com>
2024-04-16 16:01:24 +0100

    Add files via upload

---
+++ VanillaJS Loops 1/index.js
@@ -0,0 +1,11 @@
+// Iterate from 0 to 15
+for (let i = 0; i <= 15; i++) {
+    // Check if the current number is even
+    if (i % 2 === 0) {
+        console.log(i + " is even.");
+    } 
+    // If not even, it's odd
+    else {
+        console.log(i + " is odd.");
+    }
+}
